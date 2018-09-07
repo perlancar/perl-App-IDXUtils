f932a61061b99492d9d8e3274c6320aa5a04e36a
perlancar (on pc-office) <perlancar@gmail.com>
2018-09-07 20:34:36 +0700

    Initial commit

---
+++ lib/App/IDXUtils.pm
@@ -0,0 +1,16 @@
+package App::IDXUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Command-line utilities related to Indonesian Stock Exchange
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+This distribution includes several utilities related to the Indonesian Stock
+Exchange (IDX):
+
+#INSERT_EXECS_LIST
